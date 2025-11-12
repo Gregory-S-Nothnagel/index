b20cd0c3ac06eb77f51823a30b69c0f9b8d43de0
Gregory-S-Nothnagel <gsnothnagel@crimson.ua.edu>
2025-11-11 21:56:10 -0600

    commit message

--- hymns/sw.js
+++ hymns/sw.js
@@ -2,12 +2,19 @@ const CACHE_NAME = 'file-cache';
 
 // Files to cache when the service worker installs
 const filesToCache = [
-  './AMAZING_GRACE.mp3',
   './ARE_YOU_WASHED_IN_THE_BLOOD.mp4',
+  './GOD_LEADS_HIS_DEAR_CHILDREN_ALONG.mp4',
   './GREAT_IS_THY_FAITHFULNESS.mp3',
   './IM_A_PILGRIM.mp3',
   './MY_SAVIOR_FIRST_OF_ALL.mp3',
-  './NOTHING_BUT_THE_BLOOD_OF_JESUS.mp3'
+  './NOTHING_BUT_THE_BLOOD_OF_JESUS.mp3',
+  './GREAT_IS_THY_FAITHFULNESS.mp3',
+  './IT_IS_WELL_WITH_MY_SOUL.mp3',
+  './TRUST_AND_OBEY.mp3',
+  './ROCK_OF_AGES.mp3',
+  './LEANING_ON_THE_EVERLASTING_ARMS.mp3',
+  './THE_SWEET_BY_AND_BY.mp3',
+  './WHAT_A_FRIEND_WE_HAVE_IN_JESUS.mp3'
 ];
 
 // Install service worker and cache necessary files
