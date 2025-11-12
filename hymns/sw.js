3d6b9ee3469c835bb64d75a44fa7fd8b9b6eb64e
Gregory-S-Nothnagel <gsnothnagel@crimson.ua.edu>
2025-11-11 18:25:08 -0600

    Add files via upload

--- hymns/sw.js
+++ hymns/sw.js
@@ -2,11 +2,9 @@ const CACHE_NAME = 'file-cache-v1';
 
 // Files to cache when the service worker installs
 const filesToCache = [
-  '/',
-  '/index.html', // Cache index.html for offline use (even though it will be served directly from the cache)
-  '/ARE_YOU_WASHED_IN_THE_BLOOD.mkv',
-  '/I\'M_A_PILGRIM.mp3',
-  '/GOD_LEADS_HIS_DEAR_CHILDREN_ALONG.mkv',
+  '/index/hymns/ARE_YOU_WASHED_IN_THE_BLOOD.mkv',
+  '/index/hymns/I\'M_A_PILGRIM.mp3',
+  '/index/hymns/GOD_LEADS_HIS_DEAR_CHILDREN_ALONG.mkv',
 ];
 
 // Install service worker and cache necessary files
